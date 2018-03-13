1694991f24f6daa77e329a839ac7d2d27e878c8a
Tomasz Dłuski <mr.toumash@gmail.com>
2018-03-13 02:14:41 +0100

    initial commit

---
+++ toumash.dailystatus/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Toggl.MonthlyStatus
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Toggl.Ultrawave;
+    using Toggl.Ultrawave.Network;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            var credentials = Credentials.WithApiToken("enter-your-api-key");
+            var togglApi = TogglApiWith(credentials);
+            var user = togglApi.User.Get();
+
+            while (true)
+            {
+                var sum = await togglApi.TimeEntries.GetAll()
+                    .SelectMany(e => e)
+                    .Where(e => e.Duration.HasValue)
+                    .Sum(e => e.Duration.Value)
+                    .Select(e => TimeSpan.FromSeconds(e));
+                Console.Write($"\bSum is:{sum.ToString().PadLeft(15)}");
+
+
+                using (var progress = new ProgressBar())
+                {
+                    const int max = 5;
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        progress.Report((double)i / max);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+
+            //.ForEachAsync(e =>
+            //{
+            //    Console.WriteLine($"{e.At} + {TimeSpan.FromSeconds(e.Duration.Value)}");
+            //});
+
+
+
+
+
+            //var timeEntriesSum = await togglApi.TimeEntries.GetAll()
+            //    .SelectMany(e => e)
+            //    .Where(e => e.At.Month == DateTime.Now.Month && e.At.Year == DateTime.Now.Year)
+            //    .Where(e => e.Duration != null)
+            //    .Select(e => e.Duration.Value);
+            //Console.WriteLine(TimeSpan.FromSeconds(timeEntriesSum));
+        }
+
+        protected static ITogglApi TogglApiWith(Credentials credentials)
+            => new TogglApi(ConfigurationFor(credentials));
+
+        private static ApiConfiguration ConfigurationFor(Credentials credentials)
+            => new ApiConfiguration(ApiEnvironment.Production, credentials, new UserAgent("Toumash console", "1"));
+    }
+}
