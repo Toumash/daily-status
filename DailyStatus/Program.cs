3b0e4891480abbbac172229706e25277b125de9e
Tomasz Dłuski <mr.toumash@gmail.com>
2018-03-14 12:11:22 +0100

    changes .net core 2 to .net 4.6.2 and adds credential manager

---
+++ DailyStatus/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace Toumash.DailyStatus
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Security;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Toggl.Ultrawave;
+    using Toggl.Ultrawave.Network;
+    using System.Reactive.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string key = string.Empty;
+            bool authorized = false;
+            ITogglApi togglApi = null;
+
+            while (!authorized)
+            {
+                var repo = new ApiKeyRepository();
+                if (repo.Get() != string.Empty)
+                {
+                    key = repo.Get();
+                }
+                else
+                {
+                    Console.WriteLine("Please supply your password");
+                    key = SecureStringToString(Password);
+                }
+
+                var credentials = Credentials.WithApiToken(key);
+                togglApi = TogglApiWith(credentials);
+                try
+                {
+                    var user = togglApi.User.Get().GetAwaiter().Wait();
+                    repo.Save(key);
+                    authorized = true;
+                }
+                catch (Toggl.Ultrawave.Exceptions.UnauthorizedException)
+                {
+                    Console.WriteLine("Unauthorized");
+                    repo.Save("");
+                }
+            }
+
+            while (true)
+            {
+                var sum = togglApi.TimeEntries.GetAll()
+                    .SelectMany(e => e)
+                    .Where(e => e.Duration.HasValue)
+                    .Sum(e => e.Duration.Value)
+                    .Select(e => TimeSpan.FromSeconds(e)).GetAwaiter().Wait();
+                Console.Write($"\rSum is:{sum.ToString().PadRight(50)}");
+
+                using (var progress = new ProgressBar())
+                {
+                    const int max = 5;
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        progress.Report((double)i / max);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        private static ITogglApi TogglApiWith(Credentials credentials)
+            => new TogglApi(ConfigurationFor(credentials));
+
+        private static ApiConfiguration ConfigurationFor(Credentials credentials)
+            => new ApiConfiguration(ApiEnvironment.Production, credentials, new UserAgent("toumash.dailystatus", "1"));
+
+        public static SecureString Password
+        {
+            get
+            {
+                var pwd = new SecureString();
+                while (true)
+                {
+                    ConsoleKeyInfo i = Console.ReadKey(true);
+                    if (i.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    else if (i.Key == ConsoleKey.Backspace)
+                    {
+                        if (pwd.Length > 0)
+                        {
+                            pwd.RemoveAt(pwd.Length - 1);
+                            Console.Write("\b \b");
+                        }
+                    }
+                    else
+                    {
+                        pwd.AppendChar(i.KeyChar);
+                        Console.Write("*");
+                    }
+                }
+                return pwd;
+            }
+        }
+
+        private static string SecureStringToString(SecureString value)
+        {
+            IntPtr valuePtr = IntPtr.Zero;
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                return Marshal.PtrToStringUni(valuePtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
+    }
+}
