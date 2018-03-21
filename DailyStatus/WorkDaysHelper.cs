aad204df93c44912325a913c595720cdfb0a09d1
toumash <mr.toumash@gmail.com>
2018-03-21 11:40:31 +0100

    adds slower, but simpler business days counter + tests

--- DailyStatus/WorkDaysHelper.cs
+++ DailyStatus/WorkDaysHelper.cs
@@ -21,57 +21,23 @@ namespace Toumash.DailyStatus
         /// <returns>Number of business days during the 'span'</returns>
         public static int BusinessDaysUntil(this DateTime firstDay, DateTime lastDay, params DateTime[] holidaysDuringWeek)
         {
-            firstDay = firstDay.Date;
-            lastDay = lastDay.Date;
-            if (firstDay > lastDay)
-            {
-                throw new ArgumentException($"Last day is before first!\nFirst day: {firstDay}\nLastDay: {lastDay}");
-            }
-
-            var span = lastDay - firstDay;
-            int businessDays = span.Days + 1;
-            int fullWeekCount = businessDays / 7;
-
-            if (fullWeekCount % 7 != 0)
-            {
-
-                int firstDayOfWeek = firstDay.DayOfWeek.GetNumberOfDayOfWeek();
-                int lastDayOfWeek = lastDay.DayOfWeek.GetNumberOfDayOfWeek();
-
-                if (lastDayOfWeek < firstDayOfWeek)
-                {
-                    lastDayOfWeek += 7;
-                }
-
-                if (firstDayOfWeek <= 6)
-                {
-                    if (lastDayOfWeek >= 7)// Both Saturday and Sunday are in the remaining time interval
-                        businessDays -= 2;
-                    else if (lastDayOfWeek >= 6)// Only Saturday is in the remaining time interval
-                        businessDays -= 1;
-                }
-                else if (firstDayOfWeek <= 7 && lastDayOfWeek >= 7)// Only Sunday is in the remaining time interval
-                    businessDays -= 1;
-            }
-
-            // subtract the weekends during the full weeks in the interval
-            businessDays -= fullWeekCount * 2;
-
-            // subtract the number of holidays during the time interval
-            foreach (DateTime holiday in holidaysDuringWeek)
-            {
-                if (firstDay <= holiday && holiday <= lastDay && holiday.DayOfWeek != DayOfWeek.Sunday && holiday.DayOfWeek != DayOfWeek.Saturday)
-                {
-                    --businessDays;
-                }
-            }
-
-            return businessDays;
+            var countOfWorkDays = GetDateRange(firstDay, lastDay)
+                .Where(day => day.DayOfWeek != DayOfWeek.Saturday && day.DayOfWeek != DayOfWeek.Sunday)
+                .Where(day => !holidaysDuringWeek.Any(h => h.Day == day.Day && h.Month == day.Month && h.Year == day.Year))
+                .Count();
+            return countOfWorkDays;
         }
 
-        private static int GetNumberOfDayOfWeek(this DayOfWeek day)
+        public static IEnumerable<DateTime> GetDateRange(DateTime startDate, DateTime endDate)
         {
-            return day == DayOfWeek.Sunday ? 7 : (int)day;
+            if (endDate < startDate)
+                throw new ArgumentException("endDate must be greater than or equal to startDate");
+
+            while (startDate <= endDate)
+            {
+                yield return startDate;
+                startDate = startDate.AddDays(1);
+            }
         }
     }
 }
---
+++ DailyStatusTests/WorkDaysHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toumash.DailyStatus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toumash.DailyStatus.Tests
+{
+    [TestClass()]
+    public class WorkDaysHelperTests
+    {
+        [TestMethod()]
+        public void GivenFullWeekFromMondayToSunday_ShouldReturn5()
+        {
+            var monday = new DateTime(2018, 03, 19);
+            var sunday = new DateTime(2018, 03, 25);
+            var actual = WorkDaysHelper.BusinessDaysUntil(monday, sunday);
+            Assert.AreEqual(5, actual);
+        }
+
+        [TestMethod()]
+        public void GivenMarchOf2018From1stOfTheMonth_ShouldReturn14OnDay21()
+        {
+            var monday = new DateTime(2018, 03, 1);
+            var sunday = new DateTime(2018, 03, 21);
+            var actual = WorkDaysHelper.BusinessDaysUntil(monday, sunday);
+            Assert.AreEqual(15, actual);
+        }
+    }
+}
