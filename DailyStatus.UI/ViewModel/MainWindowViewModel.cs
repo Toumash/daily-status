ab4db6dbb3cb0836598c436374a14ec18d611cad
toumash <mr.toumash@gmail.com>
2018-03-21 19:14:15 +0100

    adds wpf project with sample gauge

---
+++ DailyStatus.UI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyStatus.UI.ViewModel
+{
+    class MainWindowViewModel
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+            = new SeriesCollection {
+                    new LineSeries { Values = new ChartValues <double> {3,5,7,4}, StrokeDashArray = new System.Windows.Media.DoubleCollection {2} },
+                    new ColumnSeries { Values = new ChartValues <decimal> {5,6,2,7} }
+                  };
+    }
+}
